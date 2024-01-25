3fcfec22a98247d83d28a3ad50a15845d9b4d696
Tarin James <122317277+tarin-james@users.noreply.github.com>
2024-01-25 16:50:37 -0700

    homepage hamburger and map

---
+++ scripts/hamburgermenu.js
@@ -0,0 +1,19 @@
+
+
+const mainNavToggle = document.querySelector(".main-nav-toggle");
+mainNavToggle.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (mainNavToggle.classList.contains("active-menu")) {
+    mainNavToggle.classList.remove("active-menu");
+  } else {
+    mainNavToggle.classList.add("active-menu");
+  }
+});
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('.hamburger');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
