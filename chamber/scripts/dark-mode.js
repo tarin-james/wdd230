4feed90526899aeda9085025b1ca4547b781f524
Tarin James <122317277+tarin-james@users.noreply.github.com>
2024-02-10 18:13:12 -0700

    slider added

--- chamber/scripts/dark-mode.js
+++ chamber/scripts/dark-mode.js
@@ -4,18 +4,16 @@ const header = document.querySelector("header");
 const hamburger = document.querySelector(".main-nav-toggle");
 
 modeButton.addEventListener("click", () => {
-  if (modeButton.textContent.includes("🕶️")) {
+  if (modeButton.checked) {
     main.style.background = "#000";
     main.style.color = "#fff";
     header.style.background = "#000";
     header.style.color = "#fff";
     hamburger.classList.add("main-nav-toggle-white");
-    modeButton.textContent = "🔆";
   } else {
     main.style.background = "#a37c40";
     main.style.color = "#000";
     header.style.background = "#98473e";
-    modeButton.textContent = "🕶️";
     hamburger.style.borderColor = "#000";
     hamburger.classList.remove("main-nav-toggle-white");
   }
