05b5c473bee94e6a9e6be5ec36f3b6f185364a9c
Tarin James <122317277+tarin-james@users.noreply.github.com>
2024-03-02 21:34:02 -0700

    join page

---
+++ chamber/scripts/timestamp.js
@@ -0,0 +1,6 @@
+
+    window.onload = function() {
+        const timestampField = document.getElementById('timestamp');
+        const currentTime = Date.now();
+        timestampField.value = currentTime;
+    };
