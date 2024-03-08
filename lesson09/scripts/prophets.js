02e13dfa1ae7de9fca389925479a62b717029abc
Tarin James <122317277+tarin-james@users.noreply.github.com>
2024-03-07 23:09:28 -0700

    lessons

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -4,22 +4,21 @@ const cards = document.querySelector('#cards');
 async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
-    // console.table(data.prophets); 
+    console.table(data.prophets); 
     displayProphets(data.prophets);
   }
 
-
-  const displayProphets = (prophets) => {
-    // card build code goes here
+const displayProphets = (prophets) => {
+  prophets.forEach((prophet) => {
     let card = document.createElement('section');
     let fullName = document.createElement('h2'); // fill in the blank
     let portrait = document.createElement('img');
 
     // Build the h2 content out to show the prophet's full name
-    fullName.textContent = `${prophet.fullName} ____________`; // fill in the blank
+    fullName.textContent = `${prophet.name} ${prophet.lastname}`; // fill in the blank
     // Build the image portrait by setting all the relevant attributes
     portrait.setAttribute('src', prophet.imageurl);
-    portrait.setAttribute('alt', `Portrait of ${prophet.____} ______________`); // fill in the blank
+    portrait.setAttribute('alt', `Portrait of ${prophet.name + prophet.lastname}`); // fill in the blank
     portrait.setAttribute('loading', 'lazy');
     portrait.setAttribute('width', '340');
     portrait.setAttribute('height', '440');
@@ -29,4 +28,8 @@ async function getProphetData() {
     card.appendChild(portrait);
 
     cards.appendChild(card);
-  } 
+  });
+}
+
+getProphetData();
+   
